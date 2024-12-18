20ea4388c28a099d3102b109be47a8cb4cf13be6
Grigary1 <grigaryvarkey16@gmail.com>
2024-12-18 15:28:35 +0530

    added

---
+++ app/src/main/java/com/hospital/visitorapp/AddActivity.java
@@ -0,0 +1,51 @@
+package com.hospital.visitorapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddActivity extends AppCompatActivity {
+
+    Button b1,b2;
+    EditText e1,e2,e3,e4;
+    String res,fname,lname,purpose,whom;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add);
+        e1=(EditText) findViewById(R.id.fname);
+        e2=(EditText) findViewById(R.id.lname);
+        e3=(EditText) findViewById(R.id.purpose);
+        e4=(EditText) findViewById(R.id.whom);
+        b1=(Button) findViewById(R.id.submitbtn);
+        b2=(Button) findViewById(R.id.backtomain);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fname=e1.getText().toString();
+                lname=e2.getText().toString();
+                purpose=e3.getText().toString();
+                whom=e4.getText().toString();
+                res=fname+" "+lname+" "+purpose+" "+whom;
+                Toast.makeText(getApplicationContext(),res,Toast.LENGTH_LONG).show();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ob);
+            }
+        });
+    }
+}
